42c237b81dd307d1785c60a807856511d4c29424
aashimagarg <agarg@utexas.edu>
2016-02-14 23:33:34 -0600

    submission

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -8,16 +8,32 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController {
+class BusinessesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate{
 
+    @IBOutlet weak var tableView: UITableView!
     var businesses: [Business]!
+    var filtered: [Business]!
+    var searchBar = UISearchBar()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchBar.sizeToFit()
+
+        self.searchBar.delegate = self
+        
+        navigationItem.titleView = searchBar
+        
+        searchDisplayController?.displaysSearchBarInNavigationBar = true
+
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 120
 
         Business.searchWithTerm("Thai", completion: { (businesses: [Business]!, error: NSError!) -> Void in
             self.businesses = businesses
-        
+            self.tableView.reloadData()
             for business in businesses {
                 print(business.name!)
                 print(business.address!)
@@ -40,6 +56,55 @@ class BusinessesViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        if let businesses = businesses {
+        if filtered != nil {
+            return filtered!.count
+        }
+            return businesses.count
+        }
+            
+        else {
+            return 0
+        }
+        }
+    
+    
+    
+  
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("BusinessCell", forIndexPath: indexPath) as! BusinessCell
+        if filtered != nil {
+            cell.business = filtered[indexPath.row]
+        }
+        else {
+            cell.business = businesses[indexPath.row]
+
+        }
+        
+        return cell
+    }
+    
+        func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+            if searchText.isEmpty {
+                filtered = nil
+            }
+            else {
+                filtered = businesses!.filter({(business: Business) -> Bool in
+                    if (business.name)!.rangeOfString(searchText, options: .CaseInsensitiveSearch) != nil {
+                        return true
+                    }
+                    else {
+                        return false
+                    }
+                })
+            }
+            self.tableView.reloadData()
+        }
+        
+
 
     /*
     // MARK: - Navigation
